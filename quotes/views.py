19751ef42f8656d223882e333c86443688c5fc3b
Amirhosein <gooyacoder@gmail.com>
2020-09-28 18:15:44 +0330

    Upto 5_16

--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+
+# API KEY : UWEXKNCGYISWNLMJ
+# https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo
+
 def home(request):
-	return render(request, "home.html", {})
+
+	import json
+	import requests
+
+	api_request = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=UWEXKNCGYISWNLMJ')
+	api = json.loads(api_request.content)
+
+	return render(request, "home.html", {'api': api})
 
 def about(request):
 	return render(request, "about.html", {})
