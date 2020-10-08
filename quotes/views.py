875b4c2b54d2f5c20972a430f10e0df0ef4fa9f5
Amirhosein <gooyacoder@gmail.com>
2020-10-08 19:55:08 +0330

    Upto 5_21

--- quotes/views.py
+++ quotes/views.py
@@ -10,10 +10,23 @@ def home(request):
 	# API KEY : e759076faa64ada2e1d120b9fdd68771
 	# Company Quote : https://financialmodelingprep.com/api/v3/quote/AAPL?apikey=demo
 
-	cq_request = requests.get('https://financialmodelingprep.com/api/v3/quote/AAPL?apikey=e759076faa64ada2e1d120b9fdd68771')
-	api = json.loads(cq_request.content)
 
-	return render(request, "home.html", {'api': api[0]})
+	if request.method == 'POST':
+		ticker = request.POST['ticker']
+		try:
+			cq_request = requests.get('https://financialmodelingprep.com/api/v3/quote/' + ticker + '?apikey=e759076faa64ada2e1d120b9fdd68771')
+			api = json.loads(cq_request.content)
+			return render(request, "home.html", {'api': api[0]})
+		except Exception as e:
+			api = 'Error'
+			return render(request, "home.html", {'api': api})
+
+	else:
+		return render(request, "home.html", {'ticker': 'Please enter the ticker.'})
+	
+	# api = json.loads(cq_request.content)
+
+	# return render(request, "home.html", {'api': api[0]})
 
 def about(request):
 	return render(request, "about.html", {})
